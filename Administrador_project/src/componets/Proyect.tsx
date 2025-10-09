772f4925dc5d4a506c147522afa2ca2e77e3a730
AlanEdenilson <115309252+AlanEdenilson@users.noreply.github.com>
2025-10-09 22:16:29 +0000

    creando el esqueleto

--- Administrador_project/src/componets/Proyect.tsx
+++ Administrador_project/src/componets/Proyect.tsx
@@ -3,8 +3,18 @@ const Proyect = () => {
   return (
     <div className="bg-lime-500 w-4/5 h-screen grow  gap-1 flex  flex-wrap content-start  ">
 
-       <div className="container bg-amber-400 grow h-1/5  min-w-full">
-          hola mundo
+       <div className="container bg-amber-400 grow h-1/5  min-w-full flex flex-col gap-1">
+
+          <h1 className="pt-4 ml-5 font-mono">Proyectos</h1>
+
+           <div className="ml-5 font-mono text-lg">
+            nombre del proyecto
+          </div>
+
+          <div>
+            paginas
+          </div>
+
        </div>
 
       <div className="container bg-amber-400 w-1/5 h-4/5 min-w-full">
