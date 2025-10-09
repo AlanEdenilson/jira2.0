0c9e1e1712edc6975e5a3ad2538624d6ae2aeb83
AlanEdenilson <115309252+AlanEdenilson@users.noreply.github.com>
2025-10-09 22:07:38 +0000

    creando la marquetacion

--- Administrador_project/src/componets/Proyect.tsx
+++ Administrador_project/src/componets/Proyect.tsx
@@ -1,8 +1,18 @@
 
 const Proyect = () => {
   return (
-    <div className="bg-lime-500 basis-4/5 h-screen grow">
-      hola mundo
+    <div className="bg-lime-500 w-4/5 h-screen grow  gap-1 flex  flex-wrap content-start  ">
+
+       <div className="container bg-amber-400 grow h-1/5  min-w-full">
+          hola mundo
+       </div>
+
+      <div className="container bg-amber-400 w-1/5 h-4/5 min-w-full">
+          hola mundo
+      </div>
+
+    
+      
     </div>
   )
 }
